9908ed142e1317a702a3f81136387e7ec0aec92a
Pierre Chat <pierre.chat@etu.univ-lyon1.fr>
2017-06-09 09:50:23 +0200

    Ajout d'un print qui va bien

--- src/http/server/core/Connection.java
+++ src/http/server/core/Connection.java
@@ -34,6 +34,8 @@ public class Connection implements Runnable {
                     break;
             }
             Request request = new Request(content);
+            System.out.println("***REQUEST RECEIVED***");
+            System.out.println(request);
             String filename = request.getHeader().getFile();
             if (filename.equals(""))
                 filename = "index.html";
@@ -42,9 +44,9 @@ public class Connection implements Runnable {
             Response r = new Response(h, c);
             if (request.getCookie("id") == null)
                 r.addCookie(new Cookie("id", "1574-885751-111860843"));
-            if (request.getCookie("id") == null)
+            if (request.getCookie("remember") == null)
                 r.addCookie(new Cookie("remember", "true"));
-            if (request.getCookie("id") == null)
+            if (request.getCookie("__g_") == null)
                 r.addCookie(new Cookie("__g_", "qfg4qd6g872qeg7q6d"));
             OutputStream out = socket.getOutputStream();
             out.write(r.toByteArray());
